6543205e2db337cdb30453b784081f538cef454a
Alec Chan <alec3d02@gmail.com>
2024-03-19 07:38:04 +0800

    sapphire: Build missing android.hardware.bluetooth.audio
    LegacySupport: Could not get passthrough implementation for android.hardware.bluetooth.audio@2.2::IBluetoothAudioProvidersFactory/default.
    LegacySupport: Could not get passthrough implementation for android.hardware.bluetooth.audio@2.1::IBluetoothAudioProvidersFactory/default.
    LegacySupport: Could not get passthrough implementation for android.hardware.bluetooth.audio@2.0::IBluetoothAudioProvidersFactory/default.

--- device.mk
+++ device.mk
@@ -130,7 +130,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio-impl:64
+    android.hardware.bluetooth.audio-impl:64 \
+    android.hardware.bluetooth.audio@2.0-impl:64 \
+    android.hardware.bluetooth.audio@2.1-impl:64
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
