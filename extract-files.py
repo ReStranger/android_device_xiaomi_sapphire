4fb55064f0fd340c15112802f72b00c41b2b0672
Cosmin Tanislav <demonsingur@gmail.com>
2024-10-27 15:08:31 +0200

    sapphire: Switch to python extract-utils
    Change-Id: Ia2d6a0235d9343d7de0ef69d4197cac7f276af84
    Signed-off-by: Nikita Savchenko <NikitaSavc@yandex.ru>

---
+++ extract-files.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    "device/xiaomi/sapphire",
+    "hardware/qcom-caf/sm6225",
+    "hardware/qcom-caf/wlan",
+    "hardware/xiaomi",
+    "vendor/qcom/opensource/commonsys-intf/display",
+    "vendor/qcom/opensource/dataservices",
+    "vendor/qcom/opensource/display",
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f"{lib}_{partition}" if partition == "vendor" else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    "sapphire",
+    "xiaomi",
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == "__main__":
+    utils = ExtractUtils.device(module)
+    utils.run()
