755fffbfc302a2c3743ded50c92d62809f792895
ReStranger <restranger@disroot.org>
2025-04-27 00:29:50 +0000

    sapphire: Adjust extract-files.py to LineageOS style guides
    Signed-off-by: ReStranger <restranger@disroot.org>

--- extract-files.py
+++ extract-files.py
@@ -21,18 +21,18 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    "device/xiaomi/sapphire",
-    "hardware/qcom-caf/sm6225",
-    "hardware/qcom-caf/wlan",
-    "hardware/xiaomi",
-    "vendor/qcom/opensource/commonsys-intf/display",
-    "vendor/qcom/opensource/dataservices",
-    "vendor/qcom/opensource/display",
+    'device/xiaomi/sapphire',
+    'hardware/qcom-caf/sm6225',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
 ]
 
 
-def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
-    return f"{lib}_{partition}" if partition == "vendor" else None
+def lib_fixup_vendor_suffix( lib: str, partition: str, *args, **kwargs,): # fmt: skip
+    return ( f'{lib}_{partition}' if partition == 'vendor' else None) # fmt: skip
 
 
 lib_fixups: lib_fixups_user_type = {
@@ -43,13 +43,13 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    "sapphire",
-    "xiaomi",
+    'sapphire',
+    'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
 
-if __name__ == "__main__":
-    utils = ExtractUtils.device(module)
+if ( __name__ == '__main__'): # fmt: skip
+    utils = ExtractUtils.device(module) # fmt: skip
     utils.run()
