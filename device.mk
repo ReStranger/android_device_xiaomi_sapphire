SapphireTreee/device_xiaomi_sapphire

sapphire: Enable AIDL fastboot HAL
Enable default implementation of AIDL fastboot HAL by adding
android.hardware.fastboot-service.example_recovery to PRODUCT_PACKAGES

Change-Id: I6415deb855caa844fcca0e33b14aba03d351b252

device.mk
@@ -224,7 +224,7 @@ PRODUCT_PACKAGES += \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot-service.example_recover \
     fastbootd
 
 # Fastcharge

ReStranger/android_device_xiaomi_sapphire

device.mk
@@ -226,7 +226,7 @@ PRODUCT_PACKAGES += \
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot-service.example_recovery \
     fastbootd
 
 # Fastcharge