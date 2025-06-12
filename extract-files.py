a1917453781225252337a9166f0ba2e9de5ffc6a
Demon000 <demonsingur@gmail.com>
2025-06-12 18:36:09 +0300

    sapphire: Fix elf check
    Reference: I23190f0b83c9e28aa2624a7575544b0eaf5c83c8
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Signed-off-by: ReStranger <restranger@disroot.org>

--- extract-files.py
+++ extract-files.py
@@ -37,12 +37,42 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs): # fmt: s
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    (): lib_fixup_vendor_suffix,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.hardware.qccsyshal@1.0',
+        'vendor.qti.hardware.qccsyshal@1.1',
+        'vendor.qti.hardware.qccsyshal@1.2',
+        'vendor.qti.hardware.qccvndhal@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.imsrtpservice@3.1',
+    ): lib_fixup_vendor_suffix,
 }
 blob_fixups: blob_fixups_user_type = {
     ('vendor/bin/hw/android.hardware.security.keymint-service-qti', 'vendor/lib64/libqtikeymint.so'): blob_fixup()
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
-
+     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
+    'vendor/etc/init/android.hardware.gnss-aidl-service-qti.rc': blob_fixup()
+        .regex_replace('group system gps radio vendor_qti_diag vendor_ssgtzd', 'group system gps radio vendor_qti_diag'),
+    ('vendor/bin/STFlashTool', 'vendor/lib64/libstfactory-vendor.so'): blob_fixup()
+        .add_needed('libbase_shim.so'),
+    (
+        'vendor/lib64/libalLDC.so',
+        'vendor/lib64/libalhLDC.so',
+        'vendor/lib64/libmorpho_ldc.so',
+        'vendor/lib64/libmorpho_Ldc.so',
+        'vendor/lib64/libmorpho_ubwc.so',
+        'vendor/lib64/libTrueSight.so',
+    ): blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_acquire')
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_lockPlanes')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock')
 }  # fmt: skip
 
 module = ExtractUtilsModule(
