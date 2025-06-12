c05c9255f065d37cb1dc3605d627e77096c14cac
Arian <arian.kulmer@web.de>
2025-06-12 21:47:29 +0300

    sapphire: Add v2 suffix to sensors.xiaomi
    Signed-off-by: ReStranger <restranger@disroot.org>

--- device.mk
+++ device.mk
@@ -527,7 +527,7 @@ PRODUCT_PACKAGES += \
     libsensorndkbridge
     
 PRODUCT_PACKAGES += \
-    sensors.xiaomi
+    sensors.xiaomi.v2
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
