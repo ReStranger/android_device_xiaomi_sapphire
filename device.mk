a6563c1d25595898087c445e75752dffdf7bffc9
Michael Bestas <mkbestas@lineageos.org>
2025-06-25 21:06:39 +0000

    sapphire: Switch to common QTI vibrator HAL
    Change-Id: I281c6d993901ad86592d43afcac2171840998738
    Signed-off-by: ReStranger <restranger@disroot.org>

--- device.mk
+++ device.mk
@@ -613,7 +613,10 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    $(call inherit-product, hardware/xiaomi/aidl/vibrator/vibrator-vendor-product.mk)
+	vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-V1-ndk_platform.vendor
